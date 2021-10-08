58ddd37ea1f34ab9fe0ad56401e5df983cb8c692
Yoann <yoann.deblois@gmail.com>
2021-10-08 05:11:29 +0200

    Func: Added total details, and average details

--- twitch_leaks_csv_reader.py
+++ twitch_leaks_csv_reader.py
@@ -6,7 +6,7 @@ import time
 STREAMER_ID = 12345678
 
 # Change year here.
-YEAR = 21
+YEAR = 20
 
 # Change months range here.
 # Each month is the month of pay, corresponding to the previous month of stream.
@@ -14,7 +14,7 @@ YEAR = 21
 # For the year 2020, range is 1 to 12 (complete year).
 # For the year 2021, range is 1 to 10.
 FIRST_MONTH = 1
-LAST_MONTH = 10
+LAST_MONTH = 5
 
 CALENDAR = {
     "01": "January",
@@ -49,6 +49,16 @@ def extract_csv(data_file):
 
 start = time.perf_counter()
 results = []
+sum_ad_share_gross = 0
+sum_sub_share_gross = 0
+sum_bits_share_gross = 0
+sum_prime_sub_share_gross = 0
+sum_bb_rev_gross = 0
+sum_bits_developer_share_gross = 0
+sum_bits_extension_share_gross = 0
+sum_bit_share_ad_gross = 0
+sum_fuel_rev_gross = 0
+
 for i in range(FIRST_MONTH, LAST_MONTH + 1):
     month_results = []
 
@@ -89,19 +99,49 @@ for i in range(FIRST_MONTH, LAST_MONTH + 1):
         for j in range(2, len(month_results)):
             if j != 11:
                 sum_salary += int(float(month_results[j])*100)
-        print(f"Time elapsed: {elapsed_time_formatted(start)}")
-        print(f"Length of datafile: {len(imported_data)} rows")
-        imported_data = []
     results.append(sum_salary)
+    sum_ad_share_gross += int(float(month_results[2])*100)
+    sum_sub_share_gross += int(float(month_results[3])*100)
+    sum_bits_share_gross += int(float(month_results[4])*100)
+    sum_prime_sub_share_gross += int(float(month_results[7])*100)
+    sum_bb_rev_gross += int(float(month_results[10])*100)
+    sum_bits_developer_share_gross += int(float(month_results[5])*100)
+    sum_bits_extension_share_gross += int(float(month_results[6])*100)
+    sum_bit_share_ad_gross += int(float(month_results[8])*100)
+    sum_fuel_rev_gross += int(float(month_results[9])*100)
 
+    print(f"Time elapsed: {elapsed_time_formatted(start)}")
+    print(f"Length of datafile: {len(imported_data)} rows")
+    imported_data = []
     print(f"Payment of {CALENDAR[pay_month]} {pay_complete_year} "
           f"(stream month: {CALENDAR[stream_month]} {stream_complete_year}): {round(sum_salary / 100, 2)}$")
-    print(f"Details: ad_share_gross: {month_results[2]}$, sub_share_gross: {month_results[3]}$, bits_share_gross: {month_results[4]}$, prime_sub_share_gross: {month_results[7]}$, bb_rev_gross: {month_results[10]}$, bits_developer_share_gross: {month_results[5]}$, bits_extension_share_gross: {month_results[6]}$, bit_share_ad_gross: {month_results[8]}$, fuel_rev_gross: {month_results[9]}$")
+    print(f"Details: ad_share_gross: {month_results[2]}$, sub_share_gross: {month_results[3]}$, bits_share_gross: {month_results[4]}$, "
+          f"prime_sub_share_gross: {month_results[7]}$, bb_rev_gross: {month_results[10]}$, bits_developer_share_gross: {month_results[5]}$, "
+          f"bits_extension_share_gross: {month_results[6]}$, bit_share_ad_gross: {month_results[8]}$, fuel_rev_gross: {month_results[9]}$")
     print()
 
 total = round(sum(results)/100, 2)
 nb_of_months = LAST_MONTH - FIRST_MONTH + 1
 average = round(total/nb_of_months, 2)
+sum_ad_share_gross = round(sum_ad_share_gross/100, 2)
+sum_sub_share_gross = round(sum_sub_share_gross/100, 2)
+sum_bits_share_gross = round(sum_bits_share_gross/100, 2)
+sum_prime_sub_share_gross = round(sum_prime_sub_share_gross/100, 2)
+sum_bb_rev_gross = round(sum_bb_rev_gross/100, 2)
+sum_bits_developer_share_gross = round(sum_bits_developer_share_gross/100, 2)
+sum_bits_extension_share_gross = round(sum_bits_extension_share_gross/100, 2)
+sum_bit_share_ad_gross = round(sum_bit_share_ad_gross/100, 2)
+sum_fuel_rev_gross = round(sum_fuel_rev_gross/100, 2)
+
 print(f"Total pay of {CALENDAR[first_month]} to {CALENDAR[pay_month]} {pay_complete_year} ({nb_of_months} months): {total}$")
 print(f"Average month pay: {average}$")
+print(f"Total details: ad_share_gross: {sum_ad_share_gross}$, sub_share_gross: {sum_sub_share_gross}$, bits_share_gross: {sum_bits_share_gross}$, "
+      f"prime_sub_share_gross: {sum_prime_sub_share_gross}$, bb_rev_gross: {sum_bb_rev_gross}$, "
+      f"bits_developer_share_gross: {sum_bits_developer_share_gross}$, bits_extension_share_gross: {sum_bits_extension_share_gross}$, "
+      f"bit_share_ad_gross: {sum_bit_share_ad_gross}$, fuel_rev_gross: {sum_fuel_rev_gross}$")
+print(f"Average details: ad_share_gross: {round(sum_ad_share_gross/nb_of_months, 2)}$, sub_share_gross: {round(sum_sub_share_gross/nb_of_months, 2)}$, "
+      f"bits_share_gross: {round(sum_bits_share_gross/nb_of_months, 2)}$, prime_sub_share_gross: {round(sum_prime_sub_share_gross/nb_of_months, 2)}$, "
+      f"bb_rev_gross: {round(sum_bb_rev_gross/nb_of_months, 2)}$, bits_developer_share_gross: {round(sum_bits_developer_share_gross/nb_of_months, 2)}$, "
+      f"bits_extension_share_gross: {round(sum_bits_extension_share_gross/nb_of_months, 2)}$, bit_share_ad_gross: {round(sum_bit_share_ad_gross/nb_of_months, 2)}$, "
+      f"fuel_rev_gross: {round(sum_fuel_rev_gross/nb_of_months, 2)}$")
 print(f"Duration of Analysis: {elapsed_time_formatted(start)}")
